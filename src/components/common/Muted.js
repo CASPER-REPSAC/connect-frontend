0688bb7727923711302cda674fad5acf9579ec78
5thofnovmbr <5thofnovmbr@gmail.com>
2022-03-23 21:09:17 +0900

    Delete logo.png, Change Muted comp
    + Muted component now can add custom className

--- src/components/common/Muted.js
+++ src/components/common/Muted.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { returnUnionedClassName, log } from "#serv";
 
-export function Muted({ children }) {
+export function Muted({ children, className }) {
   return (
-    <span className="font-base ml-2 text-text-500 text-xs leading-6 flex-none">
+    <span
+      className={returnUnionedClassName(
+        "font-base ml-2 text-text-500 text-xs leading-6 flex-none",
+        className
+      )}
+    >
       {children}
     </span>
   );
